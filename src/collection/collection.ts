86163dd2fce0be4e7eb6f202daef20448253b995
Bakh <imoddinov@gmail.com>
2026-09-21 21:23:38 +0500

    ref: correct methods for Collection

--- src/collection/collection.ts
+++ src/collection/collection.ts
@@ -21,14 +21,14 @@ export class Collection<T> {
 
   removeById(id: number): void {
     this._checkExistEl(id);
-    this._items = this._items.filter((_, index) => index !== id);
+    this._items.splice(id, 1);
   }
 
   replace(id: number, newItem: T): void {
     if (id < 0 || id > this._items.length) {
       throw new Error(`Элемент по индексу ${id} не существует.`);
     }
-    this._items = [...this._items.slice(0, id), newItem, ...this._items.slice(id + 1)];
+    this._items.splice(id, 1, newItem);
   }
 
   private _checkExistEl(id: number): void {
